4fbf4fe40e32f6652662d51afdbd8050cccd7145
Andrea Angelo D'Onorio De Meo <andgelorea@gmail.com>
2023-05-29 16:51:01 +0200

    fix link svg credits

--- devyour/src/pages/Credits.jsx
+++ devyour/src/pages/Credits.jsx
@@ -29,13 +29,13 @@ function Credits() {
                             <p className="mt-2 text-grey capitalize dark:text-white group-hover:text-grey rounded-md">Full Stack Developer</p>
 
                             <div className="flex mt-3 -mx-2 rounded-md">
-                                <a href="#" className="mx-2 text-grey dark:text-white hover:text-grey dark:hover:text-white group-hover:text-grey" ariaLabel="Reddit">
+                                <a href="#" className="mx-2 text-grey dark:text-white hover:text-grey dark:hover:text-white group-hover:text-grey" ariaLabel="DevYour">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
                                     </svg>
                                 </a>
 
-                                <a href="#" className="mx-2 text-grey dark:text-white hover:text-grey dark:hover:text-white group-hover:text-grey" ariaLabel="Facebook">
+                                <a href="#" className="mx-2 text-grey dark:text-white hover:text-grey dark:hover:text-white group-hover:text-grey" ariaLabel="Linkedin">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="w-6 h-6" viewBox="0 0 16 16">
                                         <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
                                     </svg>
