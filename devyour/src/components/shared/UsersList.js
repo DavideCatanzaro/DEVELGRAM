960c56596cd44ad2d5ab9cea141908f8e45d243b
Andrea Angelo D'Onorio De Meo <andgelorea@gmail.com>
2023-05-30 23:25:18 +0200

    general fix

--- devyour/src/components/shared/UsersList.js
+++ devyour/src/components/shared/UsersList.js
@@ -1,64 +1,117 @@
 const UsersList = () => {
     return (
         <>
-            <div className="flex items-center justify-center bg-gradient-to-t from-[#e7e9fe] via-[#c8ebfd] to-[#e7e9fe] h-screen p-6">
-                <div className="flex flex-col border-gray-300 border bg-white divide-y rounded-lg flex-none w-full md:w-1/2 lg:w-1/3">
-                    <div className="flex flex-col space-y-2 divide-y">
-                        {/* <!-- Item --> */}
-                        <div className="flex justify-between space-x-6 items-center p-6">
-                            <div className="flex items-center space-x-4">
-                                <img src="https://flowbite.com/docs/images/people/profile-picture-1.jpg" className="rounded-full h-14 w-14" alt="" />
-                                    <div className="flex flex-col space-y-2">
-                                        <span>Leonard Krashner</span>
-                                        <span>@Leonardkrashner</span>
-                                    </div>
-
+            <div className='flex flex-col rounded-[20px] items-center justify-center min-h-screen p-4 bg-slate-200'>
+                <div className='user-list w-full max-w-lg mx-auto bg-white rounded-xl shadow-xl flex flex-col py-4'>
+                    {/* <!--User row --> */}
+                    <div className="user-row flex flex-col items-center justify-between cursor-pointer  p-4 duration-300 sm:flex-row sm:py-4 sm:px-8 hover:bg-[#f6f8f9]">
+                        <div className="user flex items-center text-center flex-col sm:flex-row sm:text-left">
+                            <div className="avatar-content mb-2.5 sm:mb-0 sm:mr-2.5">
+                                <img className="avatar w-20 h-20 rounded-full" src="https://randomuser.me/api/portraits/men/32.jpg" />
                             </div>
-                            <div>
-                                <button className="border rounded-md px-4 py-2">
-                                    View
-                                </button>
+                            <div className="user-body flex flex-col mb-4 sm:mb-0 sm:mr-4">
+                                <a href="#" className="title font-medium no-underline">@wade_warren</a>
+                                <div className="skills flex flex-col">
+                                    <span className="subtitle text-slate-500">Wade Warren</span>
+                                </div>
                             </div>
                         </div>
-                        {/* <!-- Item --> */}
-
-                        {/* <!-- Item --> */}
-                        <div className="flex justify-between space-x-6 items-center p-6">
-                            <div className="flex items-center space-x-4">
-                                <img src="https://flowbite.com/docs/images/people/profile-picture-2.jpg" className="rounded-full h-14 w-14" alt="" />
-                                    <div className="flex flex-col space-y-2">
-                                        <span>Flyod Miles</span>
-                                        <span>@flyodmiles</span>
-                                    </div>
-                            </div>
-                            <div>
-                                <button className="border rounded-md px-4 py-2">
-                                    View
-                                </button>
-                            </div>
+                        {/* <!--Button content --> */}
+                        <div className="user-option mx-auto sm:ml-auto sm:mr-0">
+                            <button className="btn inline-block select-none no-underline align-middle cursor-pointer whitespace-nowrap px-4 py-1.5 rounded text-base font-medium leading-6 tracking-tight text-white text-center border-0 bg-blue hover:bg-pink duration-300" type="button">Follow</button>
                         </div>
-                        {/* <!-- Item --> */}
-
-                        {/* <!-- Item --> */}
-                        <div className="flex justify-between space-x-6 items-center p-6">
-                            <div className="flex items-center space-x-4">
-                                <img src="https://flowbite.com/docs/images/people/profile-picture-3.jpg" className="rounded-full h-14 w-14" alt="" />
-                                    <div className="flex flex-col space-y-2">
-                                        <span>Emily Selman</span>
-                                        <span>@emilyselman</span>
-                                    </div>
-                            </div>
-                            <div>
-                                <button className="border rounded-md px-4 py-2">
-                                    View
-                                </button>
-                            </div>
-                        </div>
-                        {/* <!-- Item --> */}
+                        {/* <!--Close Button content --> */}
                     </div>
-                    <div className="p-4">
-                        <button className="w-full border p-2 rounded-md hover:opacity-60 transition">View all</button>
+                    {/* <!--User row -->
+
+        <!--User row --> */}
+                    <div className="user-row flex flex-col items-center justify-between cursor-pointer  p-4 duration-300 sm:flex-row sm:py-4 sm:px-8 hover:bg-[#f6f8f9]">
+                        <div className="user flex items-center text-center flex-col sm:flex-row sm:text-left">
+                            <div className="avatar-content mb-2.5 sm:mb-0 sm:mr-2.5">
+                                <img className="avatar w-20 h-20 rounded-full" src="https://randomuser.me/api/portraits/women/44.jpg" />
+                            </div>
+                            <div className="user-body flex flex-col mb-4 sm:mb-0 sm:mr-4">
+                                <a href="#" className="title font-medium no-underline">@loura_weber</a>
+                                <div className="skills flex flex-col">
+                                    <span className="subtitle text-slate-500">Loura Weber</span>
+                                </div>
+                            </div>
+                        </div>
+                        {/* <!--Button content --> */}
+                        <div className="user-option mx-auto sm:ml-auto sm:mr-0">
+                            <button className="btn inline-block select-none no-underline align-middle cursor-pointer whitespace-nowrap px-4 py-1.5 rounded text-base font-medium leading-6 tracking-tight text-white text-center border-0 bg-blue hover:bg-pink duration-300" type="button">Follow</button>
+                        </div>
+                        {/* <!--Close Button content --> */}
                     </div>
+                    {/* <!--User row -->
+
+        <!--User row --> */}
+                    <div className="user-row flex flex-col items-center justify-between cursor-pointer  p-4 duration-300 sm:flex-row sm:py-4 sm:px-8 hover:bg-[#f6f8f9]">
+                        <div className="user flex items-center text-center flex-col sm:flex-row sm:text-left">
+                            <div className="avatar-content mb-2.5 sm:mb-0 sm:mr-2.5">
+                                <img className="avatar w-20 h-20 rounded-full" src="https://randomuser.me/api/portraits/men/46.jpg" />
+                            </div>
+                            <div className="user-body flex flex-col mb-4 sm:mb-0 sm:mr-4">
+                                <a href="#" className="title font-medium no-underline">@jane_cooper</a>
+                                <div className="skills flex flex-col">
+                                    <span className="subtitle text-slate-500">Jane Cooper</span>
+                                </div>
+                            </div>
+                        </div>
+                        {/* <!--Button content --> */}
+                        <div className="user-option mx-auto sm:ml-auto sm:mr-0">
+                            <button className="btn inline-block select-none no-underline align-middle cursor-pointer whitespace-nowrap px-4 py-1.5 rounded text-base font-medium leading-6 tracking-tight text-white text-center border-0 bg-blue hover:bg-pink duration-300" type="button">Follow</button>
+                        </div>
+                        {/* <!--Close Button content --> */}
+                    </div>
+                    {/* <!--User row -->
+
+        <!--User row --> */}
+                    <div className="user-row flex flex-col items-center justify-between cursor-pointer  p-4 duration-300 sm:flex-row sm:py-4 sm:px-8 hover:bg-[#f6f8f9]">
+                        <div className="user flex items-center text-center flex-col sm:flex-row sm:text-left">
+                            <div className="avatar-content mb-2.5 sm:mb-0 sm:mr-2.5">
+                                <img className="avatar w-20 h-20 rounded-full" src="https://randomuser.me/api/portraits/men/47.jpg" />
+                            </div>
+                            <div className="user-body flex flex-col mb-4 sm:mb-0 sm:mr-4">
+                                <a href="#" className="title font-medium no-underline">@guy_hawkins</a>
+                                <div className="skills flex flex-col">
+                                    <span className="subtitle text-slate-500">Guy Hawkins</span>
+                                </div>
+                            </div>
+                        </div>
+                        {/* <!--Button content --> */}
+                        <div className="user-option mx-auto sm:ml-auto sm:mr-0">
+                            <button className="btn inline-block select-none no-underline align-middle cursor-pointer whitespace-nowrap px-4 py-1.5 rounded text-base font-medium leading-6 tracking-tight text-white text-center border-0 bg-blue hover:bg-pink duration-300" type="button">Follow</button>
+                        </div>
+                        {/* <!--Close Button content --> */}
+                    </div>
+                    {/* <!--User row -->
+
+        <!--User row --> */}
+                    <div className="user-row flex flex-col items-center justify-between cursor-pointer  p-4 duration-300 sm:flex-row sm:py-4 sm:px-8 hover:bg-[#f6f8f9]">
+                        <div className="user flex items-center text-center flex-col sm:flex-row sm:text-left">
+                            <div className="avatar-content mb-2.5 sm:mb-0 sm:mr-2.5">
+                                <img className="avatar w-20 h-20 rounded-full" src="https://randomuser.me/api/portraits/women/63.jpg" />
+                            </div>
+                            <div className="user-body flex flex-col mb-4 sm:mb-0 sm:mr-4">
+                                <a href="#" className="title font-medium no-underline">@julian_jill_brown</a>
+                                <div className="skills flex flex-col">
+                                    <span className="subtitle text-slate-500">Julian Jill Brown</span>
+                                </div>
+                            </div>
+                        </div>
+                        {/* <!--Button content --> */}
+                        <div className="user-option mx-auto sm:ml-auto sm:mr-0">
+                            <button className="btn inline-block select-none no-underline align-middle cursor-pointer whitespace-nowrap px-4 py-1.5 rounded text-base font-medium leading-6 tracking-tight text-white text-center border-0 bg-blue hover:bg-pink duration-300" type="button">Follow</button>
+                        </div>
+                        {/* <!--Close Button content --> */}
+                    </div>
+                    {/* <!--User row --> */}
+
+                    <a className="show-more block w-10/12 mx-auto py-2.5 px-4 text-center no-underline rounded text-white text-center border-0 font-medium bg-blue hover:bg-pink duration-300" href="#/">
+                        
+                    Show more members  
+                    </a>
                 </div>
             </div>
         </>
