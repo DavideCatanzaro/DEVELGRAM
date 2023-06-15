452123925789678621c5d1f2f14535645ad51ea0
Lucone00 <luca.giuntoli2001.b@gmail.com>
2023-06-15 18:37:08 +0200

    get followers function

--- devyour-backend/index.js
+++ devyour-backend/index.js
@@ -1,19 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const followersData = require('./follow.json')
+
 require('dotenv').config();
 
 const app = express();
-
 app.use(bodyParser.json());
 
-// abbiamo inizializzato express e stiamo parsando la richiesta traformandola in un oggetto js
-app.get('/api/posts', (req, res, next) => {
-  //cerca nel JSON
-  console.log('mao');
-  return res.status(200).send('miao');
+app.get('/follow', (req, res) => {
+  res.json(followersData.follower);
 });
 
 const port = process.env.NODE_PORT || 3000;
 app.listen(port, () => {
-  console.log('Stiamo ascoltando sulla porta: ', port);
+  console.log('Stiamo ascoltando sulla porta: http://localhost:3000');
 });
--- devyour/src/pages/Follow.jsx
+++ devyour/src/pages/Follow.jsx
@@ -1,132 +1,152 @@
-import Separator from "../components/atoms/Separator"
-import UsersSuggested from "../components/shared/UsersSuggested"
-import ProfileCard from "../components/shared/ProfileCard"
-import Navbar from "../components/shared/Navbar"
+import Separator from "../components/atoms/Separator";
+import UsersSuggested from "../components/shared/UsersSuggested";
+import ProfileCard from "../components/shared/ProfileCard";
+import Navbar from "../components/shared/Navbar";
+import { useEffect, useState } from "react";
 
 const Follow = () => {
-    return (
-        <>
-            <div className="flex max-w-7xl mx-auto">
-                <main className="flex gap-6 container px-2 mx-auto">
-                <Navbar />
+  const [followers, setFollowers] = useState([]);
 
-                    <div className="basis-2/3 grow">
-                        <div className="sticky top-0 py-4">
-                            <div className="sticky top-0 bg-white rounded-xl shadow">
-                                <Separator title={"Follower"} />
-                            </div>
-                        </div>
+  useEffect(() => {
+    try {
+      async function fetchData() {
+        const response = await fetch("/follow");
+        const data = await response.json();
+        setFollowers(data);
+      }
+      fetchData();
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
 
-{/* Follower */}
-                        <div className="flex flex-col my-4 gap-4 bg-white justify-center items-center rounded-xl">
+  return (
+    <>
+      <div className="flex max-w-7xl mx-auto">
+        <main className="flex gap-6 container px-2 mx-auto">
+          <Navbar />
 
-                            <div className="w-full mx-5">
-                                <div className="flex flex-col bg-white shadow rounded w-full p-5 gap-3 ">
-
-                                    <div className="odd:bg-pink even:bg-blue flex gap-3 items-center font-semibold text-grey p-3 hover:bg-gradient-to-r from-[#80d8ff] to-[#ea80fc] rounded-xl hover:cursor-pointer">
-                                        <img className="w-16 h-16 rounded-full" src="https://randomuser.me/api/portraits/women/24.jpg" alt="Rebecca Burke" />
-                                        <div className="flex flex-col">
-                                            <div>Rebecca Burke</div>
-                                            <div className="text-gray-400 text-sm font-normal">+1(227)-691-8675</div>
-                                        </div>
-                                    </div>
-
-                                    <div className="odd:bg-pink even:bg-blue flex gap-3 items-center font-semibold text-grey p-3 hover:bg-gradient-to-r from-[#80d8ff] to-[#ea80fc] rounded-xl hover:cursor-pointer">
-                                        <img className="w-16 h-16 rounded-full" src="https://randomuser.me/api/portraits/men/7.jpg" alt="Connor Bell" />
-                                        <div className="flex flex-col">
-                                            <div>Connor Bell</div>
-                                            <div className="text-gray-400 text-sm font-normal">+1(579)-416-9946</div>
-                                        </div>
-                                    </div>
-
-                                    <div className="odd:bg-pink even:bg-blue flex gap-3 items-center font-semibold text-grey p-3 hover:bg-gradient-to-r from-[#80d8ff] to-[#ea80fc] rounded-xl hover:cursor-pointer">
-                                        <img className="w-16 h-16 rounded-full" src="https://randomuser.me/api/portraits/men/9.jpg" alt="Lee Fields" />
-                                        <div className="flex flex-col">
-                                            <div>Lee Fields</div>
-                                            <div className="text-gray-400 text-sm font-normal">+1(737)-996-6407</div>
-                                        </div>
-                                    </div>
-
-                                    <div className="odd:bg-pink even:bg-blue flex gap-3 items-center font-semibold text-grey p-3 hover:bg-gradient-to-r from-[#80d8ff] to-[#ea80fc] rounded-xl hover:cursor-pointer">
-                                        <img className="w-16 h-16 rounded-full" src="https://randomuser.me/api/portraits/women/22.jpg" alt="Marie Cole" />
-                                        <div className="flex flex-col">
-                                            <div>Marie Cole</div>
-                                            <div className="text-gray-400 text-sm font-normal">+1(924)-106-8494</div>
-                                        </div>
-                                    </div>
-
-                                    <div className="odd:bg-pink even:bg-blue flex gap-3 items-center font-semibold text-grey p-3 hover:bg-gradient-to-r from-[#80d8ff] to-[#ea80fc] rounded-xl hover:cursor-pointer">
-                                        <img className="w-16 h-16 rounded-full" src="https://randomuser.me/api/portraits/women/43.jpg" alt="Penny Foster" />
-                                        <div className="flex flex-col">
-                                            <div>Penny Foster</div>
-                                            <div className="text-gray-400 text-sm font-normal">+1(040)-121-3669</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-{/* Following */}
-                        <div className="flex flex-col my-4 gap-4 bg-white justify-center items-center rounded-xl">
-
-                            <div className="w-full mx-5">
-                                <div className="flex flex-col bg-white shadow rounded w-full p-5 gap-3 ">
-
-                                    <div className="odd:bg-pink even:bg-blue flex gap-3 items-center font-semibold text-grey p-3 hover:bg-gradient-to-r from-[#80d8ff] to-[#ea80fc] rounded-xl hover:cursor-pointer">
-                                        <img className="w-16 h-16 rounded-full" src="https://randomuser.me/api/portraits/women/24.jpg" alt="Rebecca Burke" />
-                                        <div className="flex flex-col">
-                                            <div>Rebecca Burke</div>
-                                            <div className="text-gray-400 text-sm font-normal">+1(227)-691-8675</div>
-                                        </div>
-                                    </div>
-
-                                    <div className="odd:bg-pink even:bg-blue flex gap-3 items-center font-semibold text-grey p-3 hover:bg-gradient-to-r from-[#80d8ff] to-[#ea80fc] rounded-xl hover:cursor-pointer">
-                                        <img className="w-16 h-16 rounded-full" src="https://randomuser.me/api/portraits/men/7.jpg" alt="Connor Bell" />
-                                        <div className="flex flex-col">
-                                            <div>Connor Bell</div>
-                                            <div className="text-gray-400 text-sm font-normal">+1(579)-416-9946</div>
-                                        </div>
-                                    </div>
-
-                                    <div className="odd:bg-pink even:bg-blue flex gap-3 items-center font-semibold text-grey p-3 hover:bg-gradient-to-r from-[#80d8ff] to-[#ea80fc] rounded-xl hover:cursor-pointer">
-                                        <img className="w-16 h-16 rounded-full" src="https://randomuser.me/api/portraits/men/9.jpg" alt="Lee Fields" />
-                                        <div className="flex flex-col">
-                                            <div>Lee Fields</div>
-                                            <div className="text-gray-400 text-sm font-normal">+1(737)-996-6407</div>
-                                        </div>
-                                    </div>
-
-                                    <div className="odd:bg-pink even:bg-blue flex gap-3 items-center font-semibold text-grey p-3 hover:bg-gradient-to-r from-[#80d8ff] to-[#ea80fc] rounded-xl hover:cursor-pointer">
-                                        <img className="w-16 h-16 rounded-full" src="https://randomuser.me/api/portraits/women/22.jpg" alt="Marie Cole" />
-                                        <div className="flex flex-col">
-                                            <div>Marie Cole</div>
-                                            <div className="text-gray-400 text-sm font-normal">+1(924)-106-8494</div>
-                                        </div>
-                                    </div>
-
-                                    <div className="odd:bg-pink even:bg-blue flex gap-3 items-center font-semibold text-grey p-3 hover:bg-gradient-to-r from-[#80d8ff] to-[#ea80fc] rounded-xl hover:cursor-pointer">
-                                        <img className="w-16 h-16 rounded-full" src="https://randomuser.me/api/portraits/women/43.jpg" alt="Penny Foster" />
-                                        <div className="flex flex-col">
-                                            <div>Penny Foster</div>
-                                            <div className="text-gray-400 text-sm font-normal">+1(040)-121-3669</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-
-                    <aside className="basis-1/6 hidden md:block">
-                        <div className="sticky top-4">
-                            <ProfileCard />
-                            <UsersSuggested />
-                        </div>
-                    </aside>
-                </main>
+          <div className="basis-2/3 grow">
+            <div className="sticky top-0 py-4">
+              <div className="sticky top-0 bg-white rounded-xl shadow">
+                <Separator title={"Follower"} />
+              </div>
             </div>
-        </>
-    )
-}
 
-export default Follow
+            {/* Follower */}
+            <div className="flex flex-col my-4 gap-4 bg-white justify-center items-center rounded-xl">
+              <div className="w-full mx-5">
+                <div className="flex flex-col bg-white shadow rounded w-full p-5 gap-3">
+                  {followers.map((follower, index) => (
+                    <div
+                      key={index}
+                      className={`odd:bg-pink even:bg-blue flex gap-3 items-center font-semibold text-grey p-3 hover:bg-gradient-to-r from-[#80d8ff] to-[#ea80fc] rounded-xl hover:cursor-pointer`}
+                    >
+                      <img
+                        className="w-16 h-16 rounded-full"
+                        src={follower.imgProfile}
+                        alt={`${follower.username}`}
+                      />
+                      <div className="flex flex-col">
+                        <div>{follower.username}</div>
+                        <div className="text-gray-400 text-sm font-normal">
+                          {follower.number}
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+
+            {/* Following */}
+            <div className="flex flex-col my-4 gap-4 bg-white justify-center items-center rounded-xl">
+              <div className="w-full mx-5">
+                <div className="flex flex-col bg-white shadow rounded w-full p-5 gap-3 ">
+                  <div className="odd:bg-pink even:bg-blue flex gap-3 items-center font-semibold text-grey p-3 hover:bg-gradient-to-r from-[#80d8ff] to-[#ea80fc] rounded-xl hover:cursor-pointer">
+                    <img
+                      className="w-16 h-16 rounded-full"
+                      src="https://randomuser.me/api/portraits/women/24.jpg"
+                      alt="Rebecca Burke"
+                    />
+                    <div className="flex flex-col">
+                      <div>Rebecca Burke</div>
+                      <div className="text-gray-400 text-sm font-normal">
+                        +1(227)-691-8675
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="odd:bg-pink even:bg-blue flex gap-3 items-center font-semibold text-grey p-3 hover:bg-gradient-to-r from-[#80d8ff] to-[#ea80fc] rounded-xl hover:cursor-pointer">
+                    <img
+                      className="w-16 h-16 rounded-full"
+                      src="https://randomuser.me/api/portraits/men/7.jpg"
+                      alt="Connor Bell"
+                    />
+                    <div className="flex flex-col">
+                      <div>Connor Bell</div>
+                      <div className="text-gray-400 text-sm font-normal">
+                        +1(579)-416-9946
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="odd:bg-pink even:bg-blue flex gap-3 items-center font-semibold text-grey p-3 hover:bg-gradient-to-r from-[#80d8ff] to-[#ea80fc] rounded-xl hover:cursor-pointer">
+                    <img
+                      className="w-16 h-16 rounded-full"
+                      src="https://randomuser.me/api/portraits/men/9.jpg"
+                      alt="Lee Fields"
+                    />
+                    <div className="flex flex-col">
+                      <div>Lee Fields</div>
+                      <div className="text-gray-400 text-sm font-normal">
+                        +1(737)-996-6407
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="odd:bg-pink even:bg-blue flex gap-3 items-center font-semibold text-grey p-3 hover:bg-gradient-to-r from-[#80d8ff] to-[#ea80fc] rounded-xl hover:cursor-pointer">
+                    <img
+                      className="w-16 h-16 rounded-full"
+                      src="https://randomuser.me/api/portraits/women/22.jpg"
+                      alt="Marie Cole"
+                    />
+                    <div className="flex flex-col">
+                      <div>Marie Cole</div>
+                      <div className="text-gray-400 text-sm font-normal">
+                        +1(924)-106-8494
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="odd:bg-pink even:bg-blue flex gap-3 items-center font-semibold text-grey p-3 hover:bg-gradient-to-r from-[#80d8ff] to-[#ea80fc] rounded-xl hover:cursor-pointer">
+                    <img
+                      className="w-16 h-16 rounded-full"
+                      src="https://randomuser.me/api/portraits/women/43.jpg"
+                      alt="Penny Foster"
+                    />
+                    <div className="flex flex-col">
+                      <div>Penny Foster</div>
+                      <div className="text-gray-400 text-sm font-normal">
+                        +1(040)-121-3669
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <aside className="basis-1/6 hidden md:block">
+            <div className="sticky top-4">
+              <ProfileCard />
+              <UsersSuggested />
+            </div>
+          </aside>
+        </main>
+      </div>
+    </>
+  );
+};
+
+export default Follow;
