7999d5e6a6e5cfc6af731c415921f2aad19a4df9
Pietro Amato <pieamato@live.it>
2023-06-21 10:17:58 +0200

    add backend map posts

--- devyour/src/pages/Devyour.jsx
+++ devyour/src/pages/Devyour.jsx
@@ -5,8 +5,30 @@ import Shoots from "../components/shared/Shoots";
 import Post2 from "../components/shared/Post2";
 import ProfileCard from "../components/shared/ProfileCard";
 import Navbar from "../components/shared/Navbar";
+import { useEffect, useState } from "react";
 
 function DevYour() {
+
+  const [posts, setPosts] = useState([]);
+  
+  async function fetchPosts() {
+    try {
+      const response = await fetch("http://localhost:3000/api/posts");
+      const postData = await response.json()
+      console.log(postData)
+      setPosts(postData);
+    
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  
+  useEffect(() => {
+     fetchPosts()
+  }, []);
+
+
+
   return (
     <>
       <div className="flex max-w-7xl mx-auto">
@@ -23,35 +45,15 @@ function DevYour() {
               <div className="sticky top-0 w-full bg-white rounded-xl shadow">
                 <div className="sticky top-0"></div>
               </div>
-
               <NewContent />
-              <Post
-                imgProfile="https://randomuser.me/api/portraits/men/9.jpg"
-                nameProfile="Pietro Amato"
-                imgPost="https://picsum.photos/500/500"
-                descriptionPost="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-              />
-              <Post2
-                imgProfile="./assets/profile-img/Andrea-profile.jpg"
-                nameProfile="Andrea A. D'Onorio De Meo"
-                imgPost="https://picsum.photos/500/250"
-                descriptionPost="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-                likes="709"
-              />
-              <Post
-                imgProfile="https://randomuser.me/api/portraits/men/7.jpg"
-                nameProfile="Davide Catanzaro"
-                imgPost="https://picsum.photos/450/300"
-                descriptionPost="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-                likes="709"
-              />
-              <Post
-                imgProfile="https://randomuser.me/api/portraits/men/6.jpg"
-                nameProfile="Maurilio Farina"
-                imgPost="https://picsum.photos/500/350"
-                descriptionPost="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-                likes="709"
-              />
+              {posts.map((item) => {
+                return ( <Post2
+                imgProfile={item.imgProfile}
+                nameProfile={item.nameProfile}
+                imgPost={item.imgPost}
+                descriptionPost={item.descriptionPost}
+              />)
+              })}
             </div>
           </div>
 
