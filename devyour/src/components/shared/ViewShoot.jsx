42bd58c08efa581a56dfc2b1b837f3710c7faf98
Lucone00 <luca.giuntoli2001.b@gmail.com>
2023-06-23 13:00:54 +0200

    setTimeOut Shhots

--- devyour/src/components/shared/ViewShoot.jsx
+++ devyour/src/components/shared/ViewShoot.jsx
@@ -1,20 +1,50 @@
-const ViewShoot = ({ viewShoot }) => {
-    return (
-        <>
-            <div className="fixed top-0 h-screen w-full z-30 flex justify-center items-center bg-black/75">
-                <div className="absolute top-10 items-center justify-center">
-                <div className="flex flex-col bg-white/75 shadow rounded-xl mb-6 p-4">
-                        <div className="flex justify-end flex-grow bg-white/5">
-                            <svg onClick={viewShoot} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 cursor-pointer">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                            </svg>
-                        </div>
-                        <img className="rounded-xl" alt="User avatar" src="https://picsum.photos/400/600?i=203" />
-                    </div>
-                </div>
-            </div>
-        </>
-    )
-}
+import React, { useState } from "react";
 
-export default ViewShoot
+const ViewShoot = ({ viewShoot }) => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
+  setTimeout(handleClose, 5000);
+
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <>
+      <div className="fixed top-0 h-screen w-full z-30 flex justify-center items-center bg-black/75">
+        <div className="absolute top-10 items-center justify-center">
+          <div className="flex flex-col bg-white/75 shadow rounded-xl mb-6 p-4">
+            <div className="flex justify-end flex-grow bg-white/5">
+              <svg
+                onClick={viewShoot}
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6 cursor-pointer"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </div>
+            <img
+              className="rounded-xl"
+              alt="User avatar"
+              src="https://picsum.photos/400/600?i=203"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ViewShoot;
