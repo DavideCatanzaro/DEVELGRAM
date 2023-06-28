61960d32b9c8f91845964ec7140591603cb12b97
Pietro Amato <pieamato@live.it>
2023-06-28 16:38:51 +0200

    create root for new post

--- devyour-backend/index.js
+++ devyour-backend/index.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-let posts = require('./post.json');
-require('dotenv').config();
-const fs = require('fs');
-const jwt = require('jsonwebtoken');
-const users = require('./users.json');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+let posts = require("./post.json");
+require("dotenv").config();
+const fs = require("fs");
+const jwt = require("jsonwebtoken");
+const users = require("./users.json");
 const followersData = require("./follow.json");
 const app = express();
 
@@ -14,53 +14,61 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(cors());
 
-app.get(`/`,(req,res)=>{
-  res.status(200).json(users)
-})
+app.get(`/`, (req, res) => {
+  res.status(200).json(users);
+});
 
-app.post('/register', (req, res) => {
-  const { firstName,lastName,username, email, password } = req.body;
-  const user = { id: users.users.length + 1,firstName,lastName,username, email, password,img: 'https://png.pngtree.com/png-vector/20191009/ourlarge/pngtree-user-icon-png-image_1796659.jpg',
-  cover: 'https://horizon-tailwind-react-git-tailwind-components-horizon-ui.vercel.app/static/media/banner.ef572d78f29b0fee0a09.png' };
+app.post("/register", (req, res) => {
+  const { firstName, lastName, username, email, password } = req.body;
+  const user = {
+    id: users.users.length + 1,
+    firstName,
+    lastName,
+    username,
+    email,
+    password,
+    img: "https://png.pngtree.com/png-vector/20191009/ourlarge/pngtree-user-icon-png-image_1796659.jpg",
+    cover:
+      "https://horizon-tailwind-react-git-tailwind-components-horizon-ui.vercel.app/static/media/banner.ef572d78f29b0fee0a09.png",
+  };
 
   // Salva il nuovo utente nel database JSON
   saveUser(user);
 
-  res.send('Registrazione completata con successo!');
-  
+  res.send("Registrazione completata con successo!");
 });
 
-
 //api/users?username=kfadfdsf
-app.get('/api/users',(req,res,next) => {
- // let firstName = req.query.firstname.toLowerCase()
-let userName = req.query.username.toLowerCase()
-let usersList = JSON.parse(fs.readFileSync('users.json')).users
-let searchResult=usersList.filter((user) => user.username.toLowerCase().includes(userName)) //|| user.firstName.toLowerCase().includes(firstName) ) 
-console.log(searchResult)
-return res.status(200).send(searchResult);
-}) 
+app.get("/api/users", (req, res, next) => {
+  // let firstName = req.query.firstname.toLowerCase()
+  let userName = req.query.username.toLowerCase();
+  let usersList = JSON.parse(fs.readFileSync("users.json")).users;
+  let searchResult = usersList.filter((user) =>
+    user.username.toLowerCase().includes(userName)
+  ); //|| user.firstName.toLowerCase().includes(firstName) )
+  console.log(searchResult);
+  return res.status(200).send(searchResult);
+});
 
 // Funzione per salvare l'utente nel database JSON
 function saveUser(user) {
   const users = loadUsers();
   users.users.push(user);
 
-  fs.writeFileSync('users.json', JSON.stringify(users));
+  fs.writeFileSync("users.json", JSON.stringify(users));
 }
 
 // Funzione per caricare gli utenti dal database JSON
 function loadUsers() {
   try {
-    const data = fs.readFileSync('users.json');
-    return JSON.parse(data)
+    const data = fs.readFileSync("users.json");
+    return JSON.parse(data);
   } catch (error) {
     return [];
   }
-  
 }
 
-app.post('/login', (req, res) => {
+app.post("/login", (req, res) => {
   const { email, password } = req.body;
   const users = loadUsers();
   const user = users.users.find(
@@ -68,25 +76,36 @@ app.post('/login', (req, res) => {
   );
 
   if (user) {
-    res.status(200).json({ message: 'Login avvenuto con successo!', user });
+    res.status(200).json({ message: "Login avvenuto con successo!", user });
   } else {
-    res.status(401).send('Credenziali non valide');
+    res.status(401).send("Credenziali non valide");
   }
 });
 
 // Funzione per caricare gli utenti dal database JSON
 function loadUsers() {
   try {
-    const data = fs.readFileSync('users.json');
+    const data = fs.readFileSync("users.json");
     return JSON.parse(data);
   } catch (error) {
-    console.error('Errore durante la lettura del file users.json', error);
+    console.error("Errore durante la lettura del file users.json", error);
     return [];
   }
 }
 
-app.get('/api/posts', (req, res) => {
+app.get("/api/posts", (req, res) => {
   res.status(200).send(JSON.stringify(posts.post));
+});
+
+app.post("/api/create/post", (req, res) => {
+  const { username, imgPost, descriptionPost } = req.body;
+  const post = {
+    username,
+    imgPost: "https://picsum.photos/500/252",
+    descriptionPost,
+  };
+  posts.post.unshift(post);
+  res.status(200).send(posts);
 });
 
 //backend followers
@@ -101,5 +120,5 @@ app.get("/api/suggested-data", (req, res) => {
 const port = process.env.NODE_PORT || 3001;
 
 app.listen(port, () => {
-  console.log('Stiamo ascoltando sulla porta: ', port);
+  console.log("Stiamo ascoltando sulla porta: ", port);
 });
